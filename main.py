1b1042bcbec507522f7bb47195c73d99a18d75d3
OP-jerbe <j.erbe@oregon-physics.com>
2025-06-02 09:27:08 -0700

    Adds a docstring to TestData class

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
 import traceback
+from dataclasses import dataclass
+
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.graph_objects import Figure
 from PySide6.QtWidgets import QApplication, QFileDialog
-from dataclasses import dataclass
 
 DIRECTORY = r"C:\Users\joshua\Documents\Epoxy FT HV Tests"
 
-@ dataclass
+
+@dataclass
 class TestData:
+    """
+    dataclass to hold test info
+    """
+
     csv_title: str | None
     time: pd.Series | None
     voltage: pd.Series | None
@@ -17,7 +23,6 @@ class TestData:
 
 
 class CSVLoader:
-
     def select_csv(self) -> str:
         """
         Open a file dialog to select a CSV file.
@@ -28,15 +33,15 @@ class CSVLoader:
         Notes:
             If no file is selected, the function will return an empty string.
         """
-        
+
         # Open the file dialog
         filepath, _ = QFileDialog.getOpenFileName(
-            parent=None,                      # Parent widget, can be None
-            caption='Choose CSV Files',                   # Dialog title
-            dir=DIRECTORY,               # Initial directory
-            filter='CSV Files (*.csv);;All Files (*)'                 # Filter for file types
+            parent=None,  # Parent widget, can be None
+            caption="Choose CSV Files",  # Dialog title
+            dir=DIRECTORY,  # Initial directory
+            filter="CSV Files (*.csv);;All Files (*)",  # Filter for file types
         )
-        
+
         return filepath
 
     def load_test_data(self, filepath: str) -> TestData:
@@ -53,48 +58,69 @@ class CSVLoader:
 
         try:
             csv_title = os.path.basename(filepath)
-            df: pd.DataFrame = pd.read_csv(filepath, skiprows=[1], usecols=[5,6,7])
-            df.rename(columns={'TIME':'Time'}, inplace=True)
-            df.rename(columns={'VOLTAGE':'Voltage (kV)'}, inplace=True)
-            df.rename(columns={'AMPERE':'Current (mA)'}, inplace=True)
-            df['Time'] = pd.to_datetime(df['Time'], format='%m/%d/%Y %I:%M:%S %p')
-            df['Voltage (kV)'] = df['Voltage (kV)'].str.replace('kV', '', regex=True).astype(float)
-            df['Current (mA)'] = df['Current (mA)'].str.replace('mA', '', regex=True).astype(float)
-            return TestData(csv_title=csv_title,
-                            time=df['Time'],
-                            voltage=df['Voltage (kV)'],
-                            current=df['Current (mA)'])
+            df: pd.DataFrame = pd.read_csv(filepath, skiprows=[1], usecols=[5, 6, 7])
+            df.rename(columns={"TIME": "Time"}, inplace=True)
+            df.rename(columns={"VOLTAGE": "Voltage (kV)"}, inplace=True)
+            df.rename(columns={"AMPERE": "Current (mA)"}, inplace=True)
+            df["Time"] = pd.to_datetime(df["Time"], format="%m/%d/%Y %I:%M:%S %p")
+            df["Voltage (kV)"] = (
+                df["Voltage (kV)"].str.replace("kV", "", regex=True).astype(float)
+            )
+            df["Current (mA)"] = (
+                df["Current (mA)"].str.replace("mA", "", regex=True).astype(float)
+            )
+            return TestData(
+                csv_title=csv_title,
+                time=df["Time"],
+                voltage=df["Voltage (kV)"],
+                current=df["Current (mA)"],
+            )
         except Exception as e:
             full_traceback = traceback.format_exc()
-            print(f'Error: {e}\n{full_traceback}')
-            return TestData(csv_title=None,
-                            time=None,
-                            voltage=None,
-                            current=None)
+            print(f"Error: {e}\n{full_traceback}")
+            return TestData(csv_title=None, time=None, voltage=None, current=None)
 
 
 def plot_test_data(data: TestData) -> Figure:
     fig = go.Figure()
 
-    voltage_color = 'blue'
-    current_color = 'red'
+    voltage_color = "blue"
+    current_color = "red"
     label_style = dict(size=16, weight="bold")  # Font size and bold style
 
     # Add voltage trace
     if data.time is not None and data.voltage is not None:
-        fig.add_trace(go.Scatter(x=data.time, y=data.voltage, mode='lines', name='Voltage (kV)', line=dict(color=voltage_color)))
+        fig.add_trace(
+            go.Scatter(
+                x=data.time,
+                y=data.voltage,
+                mode="lines",
+                name="Voltage (kV)",
+                line=dict(color=voltage_color),
+            )
+        )
 
     # Add current trace
     if data.time is not None and data.current is not None:
-        fig.add_trace(go.Scatter(x=data.time, y=data.current, mode='lines', name='Current (mA)', line=dict(color=current_color), yaxis='y2'))
+        fig.add_trace(
+            go.Scatter(
+                x=data.time,
+                y=data.current,
+                mode="lines",
+                name="Current (mA)",
+                line=dict(color=current_color),
+                yaxis="y2",
+            )
+        )
 
     # Update layout for secondary y-axis
     fig.update_layout(
         title=data.csv_title,
         xaxis_title="Time",
-        yaxis=dict(title="Voltage (kV)",
-                   title_font={**label_style, "color": voltage_color},
-                   range=[0, 41],
+        yaxis=dict(
+            title="Voltage (kV)",
+            title_font={**label_style, "color": voltage_color},
+            range=[0, 41],
         ),
         yaxis2=dict(
             title="Current (mA)",
@@ -103,7 +129,7 @@ def plot_test_data(data: TestData) -> Figure:
             side="right",
             range=[0, 0.061],
         ),
-        showlegend=False
+        showlegend=False,
     )
 
     fig.show()
@@ -116,7 +142,7 @@ def save_plot(fig: Figure) -> None:
         parent=None,
         caption="Save Plot As",
         dir=DIRECTORY,
-        filter="HTML Files (*.html);;All Files (*)"
+        filter="HTML Files (*.html);;All Files (*)",
     )
 
     if filepath:
@@ -127,8 +153,8 @@ def save_plot(fig: Figure) -> None:
         print("Save operation was canceled.")
 
 
-if __name__ == '__main__':
-    QApplication([]) # this is needed to use the QFileDialog method
+if __name__ == "__main__":
+    QApplication([])  # this is needed to use the QFileDialog method
     csv_loader: CSVLoader = CSVLoader()
     filepath: str = csv_loader.select_csv()
     test_data: TestData = csv_loader.load_test_data(filepath)
